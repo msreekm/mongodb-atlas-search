125404ab2c9ab8018a5efdc5861fadaf69718fc5
Sreekumar Menon <sreekumar@roofstock.com>
2021-01-30 23:56:27 -0800

    autocomplete

--- main.js
+++ main.js
@@ -76,11 +76,15 @@ server.get("/search", async (request, response) => {
 server.get("/count", async (request, response) => {
   const markets = "";
 
-  if (!market1) {
+  if (!request.query.market1) {
     markets = `city:${request.query.market1}`;
-  } else if (!market1 && !market2) {
+  } else if (!request.query.market1 && !request.query.market2) {
     markets = `city:${request.query.market1} OR city:${request.query.market2}`;
-  } else if (!market1 && !market2 && !market3) {
+  } else if (
+    !request.query.market1 &&
+    !request.query.market2 &&
+    !request.query.market3
+  ) {
     markets = `city:${request.query.market1} OR city:${request.query.market2} OR city:${request.query.market3}`;
   }
   console.log("markets", markets);
