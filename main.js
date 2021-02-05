2968a0fb1409755e5080b54e528b89f2b09ff095
Sreekumar Menon <sreekumar@roofstock.com>
2021-02-04 23:32:47 -0800

    tune type filter

--- main.js
+++ main.js
@@ -96,6 +96,33 @@ server.get("/count", async (request, response) => {
     markets = `city:${request.query.market1}`;
   }
 
+  // ( cfs_property_type:\"xx\" OR cfs_property_type:\"House\")
+  const propertyTypes = request.query.type.split(",");
+
+  let str = "(";
+  //   console.log(str.indexOf(","));
+  if (str.indexOf(",") !== -1) {
+    // console.log("1");
+    str = ` cfs_property_type:${propertyTypes} `;
+  } else {
+    propertyTypes.forEach((type) => {
+      str = str + ` cfs_property_type:${type} OR`;
+    });
+
+    if (
+      str.indexOf(" OR") > 0 &&
+      str.indexOf(" OR") != str.lastIndexOf(" OR")
+    ) {
+      // tests for >1 character group in string
+      str = str.slice(0, str.lastIndexOf(" OR")); // lops off the last comma
+      str =
+        str.slice(0, str.lastIndexOf(" OR")) +
+        " and " +
+        str.substring(str.lastIndexOf(" OR") + 1);
+    }
+    str = str + ")";
+  }
+
   try {
     let result = await collection
       .aggregate([
@@ -107,7 +134,7 @@ server.get("/count", async (request, response) => {
                 {
                   queryString: {
                     defaultPath: "city",
-                    query: `${markets} AND cfs_property_type:${request.query.type}`,
+                    query: `${markets} AND ${str}`,
                   },
                 },
                 {
@@ -156,7 +183,32 @@ server.get("/search", async (request, response) => {
   } else if (request.query.market1 !== "") {
     markets = `city:${request.query.market1}`;
   }
+  // ( cfs_property_type:\"xx\" OR cfs_property_type:\"House\")
+  const propertyTypes = request.query.type.split(",");
 
+  let str = "(";
+  //   console.log(str.indexOf(","));
+  if (str.indexOf(",") !== -1) {
+    // console.log("1");
+    str = ` cfs_property_type:${propertyTypes} `;
+  } else {
+    propertyTypes.forEach((type) => {
+      str = str + ` cfs_property_type:${type} OR`;
+    });
+
+    if (
+      str.indexOf(" OR") > 0 &&
+      str.indexOf(" OR") != str.lastIndexOf(" OR")
+    ) {
+      // tests for >1 character group in string
+      str = str.slice(0, str.lastIndexOf(" OR")); // lops off the last comma
+      str =
+        str.slice(0, str.lastIndexOf(" OR")) +
+        " and " +
+        str.substring(str.lastIndexOf(" OR") + 1);
+    }
+    str = str + ")";
+  }
   try {
     let result = await collection
       .aggregate([
@@ -168,7 +220,7 @@ server.get("/search", async (request, response) => {
                 {
                   queryString: {
                     defaultPath: "city",
-                    query: `${markets} AND cfs_property_type:${request.query.type}`,
+                    query: `${markets} AND ${str}`,
                   },
                 },
                 {
